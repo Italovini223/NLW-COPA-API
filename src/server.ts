99852cc8fca507f03c022f03422f208a03bb6086
Italovini223 <italovinicius2018@gmail.com>
2022-11-07 11:33:06 -0300

    ✨ feat: code in aula 4

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,15 @@
 import Fastify from "fastify";
-import {PrismaClient} from '@prisma/client';
 import cors from '@fastify/cors';
-import {z} from 'zod'
-import shortUniqueId from 'short-unique-id';
 
-const prisma = new PrismaClient({
-  log: ['query'],
-});
+import jwt from "@fastify/jwt";
+
+
+import { poolRoutes } from "./routes/pool";
+import { userRoutes } from "./routes/user";
+import { authRoutes } from "./routes/auth";
+import { gameRoutes } from "./routes/game";
+import { guessRoutes } from "./routes/guess";
+
 
 
 async function bootstrap(){
@@ -18,39 +21,17 @@ async function bootstrap(){
     origin: true
   });
 
-  fastify.get("/pools/count", async () => {
-    const count = await prisma.pool.count()
-    return {count}
-  });
-
-  fastify.get("/users/count", async () => {
-    const count = await prisma.user.count()
-    return {count}
-  });
-
-  fastify.get("/guesses/count", async () => {
-    const count = await prisma.guess.count()
-    return {count}
-  });
-
-  fastify.post("/pools", async (request, reply) => {
-    const createPollBody = z.object({
-      title: z.string(),
-    });
-    const generate = new shortUniqueId({length: 6});
+  await fastify.register(jwt, {
+    secret: 'nlwcopa',
     
-    const {title} = createPollBody.parse(request.body);
-    const code = String(generate()).toUpperCase();
+  })
 
-    await prisma.pool.create({
-      data: {
-        title,
-        code
-      }
-    });
+  await fastify.register(poolRoutes)
+  await fastify.register(authRoutes)
+  await fastify.register(gameRoutes)
+  await fastify.register(guessRoutes)
+  await fastify.register(userRoutes)
 
-    return reply.status(201).send({code})
-  });
 
   await fastify.listen({port: 3333, /*host: '0.0.0.0'*/});
 }
